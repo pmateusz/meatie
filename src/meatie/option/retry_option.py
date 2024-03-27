558f6c105438c7c534524cfb16cefaec5846e63f
pmateusz <pmateusz@users.noreply.github.com>
2024-03-27 10:56:15 +0100

    fix: Reset async retry context (#15)

--- src/meatie/option/retry_option.py
+++ src/meatie/option/retry_option.py
@@ -130,6 +130,8 @@ class AsyncRetryOperator:
         last_result: Optional[T] = None
         stopped = False
         while not stopped:
+            retry_ctx.error = None
+            retry_ctx.response = None
             if retry_ctx.attempt_number > 1:
                 wait_time = self.__wait(retry_ctx)
                 if wait_time > 0.0:
