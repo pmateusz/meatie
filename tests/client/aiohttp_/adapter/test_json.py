3838f44ddbdf4f68eab51f6936e6a5ac91a8df67
pmateusz <pmateusz@users.noreply.github.com>
2024-08-13 18:46:17 +0200

    fix: Handle ContentTypeError in aiohttp integration

--- tests/client/aiohttp_/adapter/test_json.py
+++ tests/client/aiohttp_/adapter/test_json.py
@@ -62,7 +62,41 @@ async def test_can_handle_invalid_content_type(http_server: HTTPTestServer) -> N
     # THEN
     exc = exc_info.value
     assert content == exc.text
-    assert 0 == exc.response.status
+    assert HTTPStatus.OK == exc.response.status
+
+
+@pytest.mark.asyncio()
+async def test_can_handle_html_response(http_server: HTTPTestServer) -> None:
+    # GIVEN
+    content = (
+        "<html>"
+        "<head><title>504 Gateway Time-out</title></head>"
+        "<body><center><h1>504 Gateway Time-out</h1></center><hr><center>nginx</center></body>"
+        "</html>"
+    )
+
+    def handler(request: BaseHTTPRequestHandler) -> None:
+        request.send_response(HTTPStatus.GATEWAY_TIMEOUT)
+        request.send_header("Content-Type", "text/html")
+        request.end_headers()
+        request.wfile.write(content.encode("utf-8"))
+
+    http_server.handler = handler
+
+    class TestClient(Client):
+        @endpoint("/")
+        async def get_response(self) -> dict[str, str]:
+            ...
+
+    # WHEN
+    with pytest.raises(ParseResponseError) as exc_info:
+        async with TestClient(http_server.create_session()) as client:
+            await client.get_response()
+
+    # THEN
+    exc = exc_info.value
+    assert content == exc.text
+    assert HTTPStatus.GATEWAY_TIMEOUT == exc.response.status
 
 
 @pytest.mark.asyncio()
