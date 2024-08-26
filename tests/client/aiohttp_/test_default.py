9e7fffd3118abe7732c472aca4d8e9704dd7c50a
pmateusz <pmateusz@users.noreply.github.com>
2024-08-26 21:33:25 +0200

    update aiohttp

--- tests/client/aiohttp_/test_default.py
+++ tests/client/aiohttp_/test_default.py
@@ -1,14 +1,11 @@
 #  Copyright 2024 The Meatie Authors. All rights reserved.
 #  Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
 import asyncio
-from http import HTTPStatus
 from typing import Callable, Generator
 
 import aiohttp
 import pytest
-from http_test import ClientAdapter, HTTPTestServer, StatusHandler
-from meatie import Request
-from meatie.internal.types import Client
+from http_test import ClientAdapter
 from meatie_aiohttp import Client as AiohttpClient
 from suite.client import DefaultSuite
 
@@ -22,15 +19,3 @@ class TestAiohttpDefaultSuite(DefaultSuite):
     ) -> Generator[ClientAdapter, None, None]:
         with ClientAdapter(event_loop, AiohttpClient(create_client_session())) as client:
             yield client
-
-    @staticmethod
-    def test_can_handle_schema_error(http_server: HTTPTestServer, client: Client) -> None:
-        # GIVEN
-        http_server.handler = StatusHandler(HTTPStatus.OK)
-        request = Request("GET", f"unknown://localhost:{http_server.port}", params={}, headers={})
-
-        # WHEN
-        response = client.send(request)
-
-        # THEN
-        assert HTTPStatus.OK == response.status
