f0681c0485a2beb8df99a56260ac62a6ba4ee344
pmateusz <pmateusz@users.noreply.github.com>
2024-10-27 19:46:45 +0100

    wip

---
+++ tests/examples/test_wrap_client.py
@@ -0,0 +1,72 @@
+from http import HTTPStatus
+from unittest.mock import AsyncMock, Mock
+
+import pytest
+from meatie import AsyncResponse, HttpStatusError, endpoint
+
+
+class Client:
+    @endpoint("/v1/multisig-transactions/{tx_hash}")
+    async def delete_multisig_transaction(self, tx_hash: str) -> AsyncResponse:
+        ...
+
+
+class Controller:
+    def __init__(self, client: Client) -> None:
+        self._client = client
+
+    async def delete_transaction(self, tx_hash: str) -> bool:
+        response = await self._client.delete_multisig_transaction(tx_hash)
+        match response.status:
+            case HTTPStatus.NO_CONTENT:
+                return True
+            case HTTPStatus.NOT_FOUND:
+                return False
+            case _:
+                raise HttpStatusError(response)
+
+
+@pytest.mark.asyncio()
+async def test_handles_not_found() -> None:
+    # GIVEN
+    client = Mock(spec=Client)
+    client.delete_multisig_transaction = AsyncMock(return_value=Mock(status=HTTPStatus.NOT_FOUND))
+    controller = Controller(client)
+
+    # WHEN
+    is_deleted = await controller.delete_transaction("OxDEAD")
+
+    # THEN
+    assert not is_deleted
+
+
+@pytest.mark.asyncio()
+async def test_handles_no_content() -> None:
+    # GIVEN
+    client = Mock(spec=Client)
+    client.delete_multisig_transaction = AsyncMock(return_value=Mock(status=HTTPStatus.NO_CONTENT))
+    controller = Controller(client)
+
+    # WHEN
+    is_deleted = await controller.delete_transaction("Ox1234567")
+
+    # THEN
+    assert is_deleted
+
+
+@pytest.mark.asyncio()
+async def test_handles_other_status() -> None:
+    # GIVEN
+    client = Mock(spec=Client)
+    client.delete_multisig_transaction = AsyncMock(
+        return_value=Mock(status=HTTPStatus.INTERNAL_SERVER_ERROR)
+    )
+    controller = Controller(client)
+
+    # WHEN
+    with pytest.raises(HttpStatusError) as exc_info:
+        await controller.delete_transaction("Ox1234567")
+
+    # THEN
+    assert isinstance(exc_info.value, HttpStatusError)
+    assert exc_info.value.response.status == HTTPStatus.INTERNAL_SERVER_ERROR
