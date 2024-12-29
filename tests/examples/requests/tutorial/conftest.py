f0bf7ffbf7dd201e64f0871e642bf62b45fce9e3
pmateusz <pmateusz@users.noreply.github.com>
2024-12-29 12:06:21 +0100

    wip

--- tests/examples/requests/tutorial/conftest.py
+++ tests/examples/requests/tutorial/conftest.py
@@ -2,5 +2,5 @@ import sys
 
 import pytest
 
-if sys.version_info < (3, 11):
-    pytest.skip("requires Python 3.11", allow_module_level=True)
+if sys.version_info < (3, 12):
+    pytest.skip("requires Python 3.12", allow_module_level=True)
