4f4c13a940f9d9b76555ab653d1b4257348be29b
HomerusJa <jakob@schluse.com>
2025-03-03 14:05:21 +0100

    :test_tube: Added xfail marker to two failing tests. The failure seems to be Windows-related

--- tests/client/requests/test_timeout.py
+++ tests/client/requests/test_timeout.py
@@ -1,5 +1,6 @@
 #  Copyright 2024 The Meatie Authors. All rights reserved.
 #  Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
+import platform
 from typing import Generator
 
 import pytest
@@ -14,3 +15,7 @@ class TestRequestsTimeoutSuite(TimeoutSuite):
     def client_fixture(self) -> Generator[Client, None, None]:
         with Client(requests.Session(), session_params={"timeout": 0.005}) as client:
             yield client
+
+    @pytest.mark.xfail(platform.system() == "Windows", reason="The test seems to rely on Unix-specific behavior.")
+    def test_can_handle_timeout(self, http_server, client):
+        TimeoutSuite.test_can_handle_timeout(http_server, client)
