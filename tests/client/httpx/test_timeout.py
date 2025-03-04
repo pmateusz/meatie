331b62dac3c3944666ae0d1551dc9b765701cab5
pmateusz <pmateusz@users.noreply.github.com>
2025-03-04 20:05:25 +0100

    wip

--- tests/client/httpx/test_timeout.py
+++ tests/client/httpx/test_timeout.py
@@ -1,6 +1,5 @@
 #  Copyright 2024 The Meatie Authors. All rights reserved.
 #  Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
-import platform
 from typing import Generator
 
 import httpx
@@ -16,6 +15,5 @@ class TestHttpxTimeoutSuite(TimeoutSuite):
         with Client(httpx.Client(), client_params={"timeout": 0.005}) as client:
             yield client
 
-    @pytest.mark.xfail(platform.system() == "Windows", reason="The test seems to rely on Unix-specific behavior.")
     def test_can_handle_timeout(self, http_server, client):
         TimeoutSuite.test_can_handle_timeout(http_server, client)
